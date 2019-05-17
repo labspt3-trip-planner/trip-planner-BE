c0f75d75bd56530a22ab177512e5705c16279fe5
Jonathan Dorety <kyullog@gmail.com>
2019-05-16 23:17:47 -0400

    Configure database

---
+++ firebase/database/dbConfig.js
@@ -0,0 +1,9 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./service-account-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+module.exports = admin.firestore();
