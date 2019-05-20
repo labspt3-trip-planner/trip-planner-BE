83e215a5ce51289ae50847c7f0d0a1b98b2cb1ad
Jonathan Dorety <kyullog@gmail.com>
2019-05-19 22:41:48 -0400

    fix Firebase config

---
+++ firebase/config/firebase.js
@@ -0,0 +1,16 @@
+const firebase = require("firebase/app");
+
+require("firebase/auth");
+require("firebase/firestore");
+
+const fireBaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID
+};
+
+module.exports = firebase.initializeApp(fireBaseConfig);
--- firebase/database/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("./service-account-key.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin.firestore();
