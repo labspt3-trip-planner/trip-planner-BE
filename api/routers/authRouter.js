5330aa0aca0e15f150a0696868cb87762849409c
Jonathan Dorety <kyullog@gmail.com>
2019-05-21 23:05:14 -0400

    Register endpoint revised (need DB support)

--- api/routers/authRouter.js
+++ api/routers/authRouter.js
@@ -1,33 +1,17 @@
 const router = require("express").Router();
 
-const firebase = require("../../firebase/config/firebase.js");
-const db = firebase.firestore();
+const { registerUser } = require("../../firebase/auth/authHelpers.js");
 
 router.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+  const user = req.body;
   try {
-    // add user to Firebase auth
-    const register = await firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .catch(function(error) {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ...
-        console.log(errorCode, errorMessage);
-        res.status(400).json({ errorCode, errorMessage });
-      });
-    console.log(register);
-    // add user to "users" collection in Firestore DB
-    await db
-      .collection("users")
-      .doc(register.user.uid)
-      .set({ email: register.user.email });
-    res.status(201).json({
-      email: register.user.email,
-      uid: register.user.uid
-    });
+    const registered = await registerUser(user);
+    console.log("endpoint:", registered);
+    if (registered.uid) {
+      res.status(201).json(registered);
+    } else {
+      res.status(400).json(registered);
+    }
   } catch (err) {
     console.log(err);
     res
