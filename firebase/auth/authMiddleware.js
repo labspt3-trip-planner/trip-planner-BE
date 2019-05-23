2b87d8a87cf379f718f460e76278ac181e64bec3
Jesse Theisen <theiseja@gmail.com>
2019-05-22 20:54:39 -0500

    Adds authMiddleware.js and verifyToken function for login

--- firebase/auth/authMiddleware.js
+++ firebase/auth/authMiddleware.js
@@ -1,5 +1,21 @@
-async function verifyToken(req, res, next) {
-    const idToken = req.headers.authorization;
+const admin = require('firebase-admin');
 
-    
-}
+async function verifyToken(req, res, next) {
+    const userToken = req.headers.authorization;
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(userToken);
+
+        if (decodedToken) {
+            req.body.uid = decodedToken.uid;
+
+            return next();
+        } else {
+            return res.status(401).send('Not an authorized user');
+        }
+    } catch (e) {
+        return res.status(401).send('Not an authorized user');
+    };
+};
+
+module.exports = verifyToken;
