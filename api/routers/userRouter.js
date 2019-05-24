a1ad87972004bee0e46c8d8f5d09189703e5d26d
Jesse Theisen <theiseja@gmail.com>
2019-05-23 22:25:00 -0500

    Debugging in progress for user ID GET endpoint

--- api/routers/userRouter.js
+++ api/routers/userRouter.js
@@ -1,24 +1,19 @@
 const router = require("express").Router();
 
-const db = require("../../firebase/config/firebase").firestore();
+const { getByUid } = require("../../database/users/usersHelper");
 
-const {
-    addUser,
-    getByUid,
-    getByEmail
-  } = require("../../database/users/usersHelper");
 
 router.get('/users', (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:uid', (req, res) => {
     const { uid } = req.params;
-  db
-      .getByUid(uid)
+
+      getByUid(`${uid}`)
       .then(uid => {
         if (uid) {
-          res.json(uid);
+          res.json(uid)
         } else {
           res.status(404).json({ message: "User with that ID does not exist" });
         }
@@ -27,7 +22,7 @@ router.get('/:id', (req, res) => {
         res
           .status(404)
           .json({ error: "The user information could not be retrieved." });
-      });
+      })
   });
 
 module.exports = router;
