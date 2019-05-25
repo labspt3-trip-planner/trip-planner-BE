194d9741f32fb4d56c2bb8fb10900e4afa6fefbe
Jesse Theisen <theiseja@gmail.com>
2019-05-25 13:04:36 -0500

    Implants GET all users method with firebase

--- api/routers/userRouter.js
+++ api/routers/userRouter.js
@@ -2,7 +2,27 @@ const router = require("express").Router();
 
 const { getByUid } = require("../../database/users/usersHelper");
 
+const firebaseAdmin = require('../../firebase/config/firebase').auth()
 
+//firebase list all users
+function listAllUsers(nextPageToken) {
+  // lists a batch of users, 1000 at a time.
+  firebaseAdmin.listUsers(1000, nextPageToken)
+  .then(function(listUsers) {
+    listUsers.users.forEach(function(userRecord) {
+      console.log('user', userRecord.toJSON());
+    });
+    if (listUsers.pageToken) {
+      // lists the next batch
+      listAllUsers(listUsers.pageToken);
+    }
+  })
+  .catch(function(error) {
+    console.log('Error listing all the users in the DB.', error);
+  });
+  // Start listing users from the beginning of the users collection, 1000 at a time.
+}
+listAllUsers();
 router.get('/users', (req, res) => {
 const { user } = req.body;
 
