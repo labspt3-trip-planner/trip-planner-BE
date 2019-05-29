ff3862dd40f9aecbd9cb2e90aa5072cf5e15e5db
Jonathan Dorety <kyullog@gmail.com>
2019-05-28 23:58:24 -0400

    Add edit trip endpoint (PUT).  Delete works, but needs working out

--- api/routers/tripRouter.js
+++ api/routers/tripRouter.js
@@ -19,4 +19,42 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+    const updated = await db.updateTrip(id, changes);
+    console.log(updated);
+    if (updated._writeTime) {
+      res.status(200).json(updated);
+    } else {
+      res.status(404).json({ err: "Trip not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ err: "There was a problem processing your request" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await db.removeTrip(id);
+    const trip = await db.getTripById(id);
+    console.log(trip);
+    if (trip) {
+      res.status(400).json({ ...trip });
+    } else {
+      res.status(200).json({ ...trip });
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ err: "There was a problem processing your request" });
+  }
+});
+
 module.exports = router;
