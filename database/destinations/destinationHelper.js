7433922a1654ebda2bb06188f2720ffe8dc70137
Jonathan Dorety <kyullog@gmail.com>
2019-05-30 23:27:58 -0400

    Add destinations DB helpers

--- database/destinations/destinationHelper.js
+++ database/destinations/destinationHelper.js
@@ -5,6 +5,19 @@ module.exports = {
   addDestination
 };
 
-function getById(id) {}
+function getById(id) {
+  return db
+    .collection("destinations")
+    .doc(`${id}`)
+    .get()
+    .then(res => res.data())
+    .catch(err => err);
+}
 
-function addDestination(destination) {}
+function addDestination(destination) {
+  return db
+    .collection("destinations")
+    .add(destination)
+    .then(ref => ref.id)
+    .catch(err => err);
+}
