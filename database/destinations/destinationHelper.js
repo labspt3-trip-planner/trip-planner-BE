8cf42b12deb468c0d76c0bf558415a073b7d0ffd
Jonathan Dorety <kyullog@gmail.com>
2019-05-30 23:22:42 -0400

    Create destinations helper file

---
+++ database/destinations/destinationHelper.js
@@ -0,0 +1,10 @@
+const db = require("../../firebase/config/firebase.js").firestore();
+
+module.exports = {
+  getById,
+  addDestination
+};
+
+function getById(id) {}
+
+function addDestination(destination) {}
