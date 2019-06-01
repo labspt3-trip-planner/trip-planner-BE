cdf0b60e139d89f1b029a8f86331f4309c304f4a
Jesse Theisen <theiseja@gmail.com>
2019-06-01 12:08:51 -0500

    fixed an issue with code

--- api/routers/userRouter.js
+++ api/routers/userRouter.js
@@ -116,15 +116,25 @@ router.get("/:uid/trips", async (req, res) => {
   }
 });
 
+    // DELETE endpoint delete a user
+router.delete("/delete/:uid", async (req, res) => {
+  const uid = req.params;
+  try {
     if (uid) {
+      res.status(200).json({
         message: "User has been deleted"
+      });
     } else {
+      res.status(404).json({
         error: "The User with the specified ID does not exist"
+      });
     }
+  } catch (e) {
     console.log(req.params);
     res.status(500).json(e);
   }
 });
+
 
 // PUT endpoint update user info
 router.put("/edit/:uid", (req, res) => {});
