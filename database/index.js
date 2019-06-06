2c3001c5f13ba0dc9753557a916ec16f5fc9f2b3
Jonathan Dorety <kyullog@gmail.com>
2019-06-05 22:08:24 -0400

    Create master db helper import index

---
+++ database/index.js
@@ -0,0 +1,6 @@
+const dest = require("./destinations/destinationHelper.js");
+const fave = require("./favorites/faveHelpers.js");
+const trip = require("./trips/tripsHelper.js");
+const user = require("./users/usersHelper.js");
+
+module.exports = { dest, fave, trip, user };
