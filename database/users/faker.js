cb6090db34115764186d52130951dbc6320bb5ad
Jesse Theisen <theiseja@gmail.com>
2019-06-11 18:48:41 -0500

    Builds for loop for fakerjs, users are console logging but not showing in postman

--- database/users/faker.js
+++ database/users/faker.js
@@ -1,23 +1,12 @@
 const faker = require('faker');
 
-const fakeUsersAmount = 500;
-
 const User = {
-    email: faker.internet.email(),
-    password: faker.internet.password()
-}
-
-
-const generateUsers = () => {
-    let arr = [];
-
-    for(let i = 0; i < fakeUsersAmount; i++) {
-        arr.push({
-            username: faker.internet.email(),
-            password: faker.internet.password()
-        })
-    }
-}
-console.log(generateUsers);
+	email: faker.internet.email(),
+	password: faker.internet.password()
+};
+const a = new Array(500)
+	.fill(null)
+	.map(e => (e = faker.fake('{{internet.email}}, {{internet.password}}')));
+console.log(a);
 
 module.exports = User;
