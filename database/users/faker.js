9d836a50928e251c3a7bdfbde0ce1e67b5b487bc
Jesse Theisen <theiseja@gmail.com>
2019-06-11 18:25:41 -0500

    Starts building for loop and function for multiple users

--- database/users/faker.js
+++ database/users/faker.js
@@ -1,9 +1,23 @@
 const faker = require('faker');
 
+const fakeUsersAmount = 500;
+
 const User = {
     email: faker.internet.email(),
     password: faker.internet.password()
 }
-console.log(User);
+
+
+const generateUsers = () => {
+    let arr = [];
+
+    for(let i = 0; i < fakeUsersAmount; i++) {
+        arr.push({
+            username: faker.internet.email(),
+            password: faker.internet.password()
+        })
+    }
+}
+console.log(generateUsers);
 
 module.exports = User;
