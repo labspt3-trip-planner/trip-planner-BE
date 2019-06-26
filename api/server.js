c6deec4a4a4ca2429b9115c4b2e98ea0cb5d0afb
Jesse Theisen <theiseja@gmail.com>
2019-06-26 13:34:39 -0500

    Refactored stripe code

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const cool = require("cool-ascii-faces");
 const a = require('../database/users/faker');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);  // stripe secret key, I can provide account info if needed.
 
 const authRouter = require("./routers/authRouter.js");
 const userRouter = require("./routers/userRouter");
@@ -21,6 +21,57 @@ server.use("/users", userRouter);
 server.use("/trip", tripRouter);
 server.use("/favorites", favesRouter);
 
+// Stripe product, I can move later when we clean our code up before production if needed or wanted.
+const product = [
+  {
+    uuid: 1,
+    productName: 'Premium service',
+    productDescription: 'Premium service for our app',
+    productPrice: 9.99
+  }
+];
+
+// Stripe end points(can be changed if needed)
+server.get('/products/:uuid', (req, res, next) => {
+  const productID = req.params.uuid;
+  const product = products.filter((product) => {
+    return parseInt(productID) === product.uuid;
+  });
+  if (product[0]) {
+    return res.render('product', {productInfo: product[0]});
+  }
+  return res.send('Product does not exist in our database.');
+});
+
+server.post('/users/charge', (req, res, next) => {
+  const stripeToken = req.body.stripeToken;
+  const price = req.body.price;
+  const amount = req.body.price * 100;
+  const productName = req.body.name;
+  // ensure amount === to avoid fraud
+  const product = products.filter((product) => {
+    return productName === product.productName && parseFloat(price) === parseFloat(product.productPrice);
+  });
+
+  if (product[0]) {
+    stripe.charges.create({
+      card: stripeToken,
+      currency: 'usd',
+      amount: amount
+    }, (err, charge) => {
+      if(err) {
+        console.log('here');
+        res.send('error');
+      } else {
+        res.send('success');
+      }
+    });
+  } else {
+    console.log('Product name or price does not match our records.');
+    res.send('error');
+  }
+});
+
 // just makes sure the server is live and running
 server.get("/", async (req, res) => {
   res.send(`Welcome to the Trip Planner API!`);
@@ -36,14 +87,6 @@ server.get("/cool", (req, res) => {
   res.send(cool());
 });
 
-// stripe test API request
-(async () => {
-  const charge = await stripe.charges.create({
-    amount: 20,
-    currency: 'usd',
-    source: 'tok_visa',
-    receipt_email: 'theiseja@gmail.com'
-  });
-});
+// stripe checkout session
 
 module.exports = server;
