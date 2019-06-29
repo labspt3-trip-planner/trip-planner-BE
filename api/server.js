b111e7a3dc6f83f024e07705d223c92ad18193a5
Jesse Theisen <theiseja@gmail.com>
2019-06-29 10:47:45 -0500

    Fixed a typo

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const authRouter = require("./routers/authRouter.js");
 const userRouter = require("./routers/userRouter");
 const tripRouter = require("./routers/tripRouter");
 const favesRouter = require("./routers/favoritesRouter.js");
-const paymentRouter = require('./api/payments/paymentRouter');
+const paymentRouter = require('./payments/paymentRouter');
 
 
 const server = express();
