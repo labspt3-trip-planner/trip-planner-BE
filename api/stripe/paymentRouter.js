abfa4405d2dfc3d34b7f2c63d45e995429399553
Jesse Theisen <theiseja@gmail.com>
2019-07-13 11:51:14 -0500

    Saving progress, refactoring a bit

--- api/stripe/paymentRouter.js
+++ api/stripe/paymentRouter.js
@@ -1,17 +1,28 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);  // stripe secret key, I can provide account info if needed.
 const router = require('express').Router();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+  
 
-(async () => {
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        subPlan: {
-            items: [{
-                plan: 'Premium',
-            }],
-        },
-        success_url: 'https://localhost:9090/success',
-        cancel_url: 'https://localhost:9090/cancel',
+router.post('/checkout', (req, res) =>  {
+    const stripeToken = req.body.data.stripeToken;
+    const payment = Number(req.body.data.payment);
+    const subPlans = req.body.data.description;
+    const email = req.body.data.email;
+
+    (async () => {
+        try {
+            const charge = await stripe.charges.create({
+                amount: payment,
+                currency: 'usd',
+                description: subPlans,
+                source: stripeToken,
+                statement_description: subPlans,
+                receipt_email: email
+            });
+            res.status(201).json([{ charge }]);
+        } catch (err) {
+            res.status(401).json({ err })
+        }
     });
-});
+})
 
-module.exports = router;
+  module.exports = router;
