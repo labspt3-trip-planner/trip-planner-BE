21a5953606170fa2c949266b715de0a8da701bed
Jonathan Dorety <kyullog@gmail.com>
2019-07-24 18:48:35 -0700

    Code cleanup

--- firebase/auth/authMiddleware.js
+++ firebase/auth/authMiddleware.js
@@ -1,23 +1,19 @@
 const admin = require("../config/firebase");
 
-async function verifyToken(req, res, next) {
+
+function verifyToken(req, res, next) {
   const userToken = req.headers.authorization;
-  console.log("middleware");
-  try {
-    console.log(userToken)
-    const decodedToken = await admin.auth().verifyIdToken(userToken);
-
-    if (decodedToken) {
+  admin
+    .auth()
+    .verifyIdToken(userToken)
+    .then(decodedToken => {
       req.body.uid = decodedToken.uid;
-
       return next();
-    } else {
-      return res.status(401).send("Not an authorized user");
-    }
-  } catch (e) {
-    console.log(e);
-    return res.status(401).send(e.errorInfo);
-  }
+    })
+    .catch(err => {
+      console.log("Middleware error: ", err);
+      res.status(400).json({ err: "Could not verify" });
+    });
 }
 
 module.exports = verifyToken;
